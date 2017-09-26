648e7dd2d162a8dfe17b52e68d818fdc41fff068
milagrosMndoza <milagrosmndoza@gmail.com>
2017-09-26 17:27:16 -0500

    terminando mi codigo con clases constructor

--- js/Asiento.js
+++ js/Asiento.js
@@ -5,115 +5,118 @@ class pasajero {
 		this.dni = dni
 	}
 }
-class Areolinea {
-	constructor(juega) {
+class Aerolinea {
+	constructor() {
 		this.celdaPress = undefined;
 		this.asientos = new Array(32);
 		this.numAsiento = -1;
+		//this.inicio()
 	}
-	dibuja() {
-		for (var i = 0; i < this.asientos.length; i++) {
+	inicio() {
+		/** llenar asientos */
+		for (let i = 0; i < this.asientos.length; i++) {
 			this.asientos[i] = undefined;
 		}
-		var celdas = document.getElementsByTagName("td");
-		for (var i = 0; i < celdas.length; i++) {
-			celdas[i].addEventListener("click",  this.redirect, false);
-			this.limpiar();
-		}
-		this.btn();
-	}
-	btn (){
-		document.getElementById('reservar').onclick = () => this.reservar;
+		//console.log(this.asientos)
+		$('td').click((evento) => this.redirect(event));
+		$("#buscarDni").click(() => this.buscarDNI());
+		$("#reservar").click(() => this.reservar());
+		$("#listar").click(() => this.listar());
+		$("#cancelar").click(() => this.cancelar());
 	}
 	redirect(event) {
-		console.log(event);
-		document.getElementById("mostrar").innerHTML = (event.target.textContent);
+		console.log(event)
+		this.limpiar();
+		$("#enlistando").empty();
+		$("#mostrar").val(event.target.textContent);
 		this.celdaPress = event.target;
-		this.numAsiento = event.target.textContent;
+		this.numAsiento = parseInt(event.target.textContent);
 		if (this.celdaPress.style.backgroundColor == "red") {
-			document.getElementById("nombre").value = this.asientos[this.numAsiento - 1].nombre;
-			document.getElementById("apellido").value = this.asientos[this.numAsiento - 1].apellido;
-			document.getElementById("dni").value = this.asientos[this.numAsiento - 1].dni;
+			$("#nombre").val(this.asientos[this.numAsiento - 1].nombre);
+			$("#apellido").val(this.asientos[this.numAsiento - 1].apellido);
+			$("#dni").val(this.asientos[this.numAsiento - 1].dni);
 			console.log(this.celdaPress.style.backgroundColor)
 		}
 	}
-	buscar() {
-		var buscardni = document.getElementById("buscarDni").value;
-		for (var i = 0; i < this.asientos.length; i++) {
+	buscarDNI() {
+		let buscardni = $("#buscar").val();
+		for (let i = 0; i < this.asientos.length; i++) {
 			if (this.asientos[i] != undefined) {
 				if (this.asientos[i].dni == buscardni) {
-					document.getElementById("mostrar").innerHTML = i + 1;
-					document.getElementById("nombre").value = this.asientos[i].nombre;
-					document.getElementById("apellido").value = this.asientos[i].apellido;
-					document.getElementById("dni").value = this.asientos[i].dni;
+					$("#mostrar").val(i + 1);
+					$("#nombre").val(this.asientos[i].nombre);
+					$("#apellido").val(this.asientos[i].apellido);
+					$("#dni").val(this.asientos[i].dni);
 				}
 			}
 		}
 	}
 	reservar() {
-		console.log('der')
-		if (this.numAsiento == -1) {
-			alert("Seleccione primero un asiento");
-		} else {
-			var nom = document.getElementById("nombre").value;
-			var ape = document.getElementById("apellido").value;
-			var doc = document.getElementById("dni").value;
-			var pasajero = {
-				nombre: nom,
-				apellido: ape,
-				dni: doc
+		if ((this.numAsiento == -1) || $("#nombre").val() == "" || $("#apellido").val() == "" || $("#dni").val() == "") {
+			swal('Oops...', 'Selecione primero un asiento y complete todos los campos!', 'error');
+		}
+		else {
+			let nom = $("#nombre").val();
+			let ape = $("#apellido").val();
+			let doc = $("#dni").val();
+			let pasajero = {
+				nombre: nom
+				, apellido: ape
+				, dni: doc
 			};
-			alert("Asiento N° " + this.numAsiento + "\n" + "Pasajero: " + pasajero.nombre + " " + pasajero.apellido + "\n" + "Registrado correctamente!");
-			this.asientos[numAsiento - 1] = pasajero;
+			swal({
+				title: "Registrado!"
+				, text: "Asiento N° " + this.numAsiento + "\n" + "Pasajero: " + pasajero.nombre + " " + pasajero.apellido + "\n" + "DNI: " + pasajero.dni
+				, type: "success"
+			});
+			this.asientos[this.numAsiento - 1] = pasajero;
 			this.celdaPress.style.backgroundColor = "red";
-			//numAsiento=-1;
+			//this.numAsiento=-1;
 			console.log(this.asientos)
 		}
 		this.limpiar();
 	}
 	listar() {
-		var html = " ";
-		var enlistando = document.getElementById("enlistando");
-		html = "<h2>Lista de asientos reservados:</h2>" + "<table width=300; border=2>";
-		for (var i = 0; i < this.asientos.length; i++) {
+		let html1 = " ";
+		html1 = "<h2>Lista de asientos reservados:</h2><table width=300; border=2>";
+		for (let i = 0; i < this.asientos.length; i++) {
 			if (this.asientos[i] != undefined) {
-				html += "<tr>";
-				html += "<td> <b> <h3>N° de Asiento:</h3></b></td><td>" + "<br><h3><b>" + (i + 1) + "</b></h3>" + "<br></td></tr>";
-				html += "<td> <b> Nombre: </b></td><td>" + this.asientos[i].nombre + "<br></td></tr>";
-				html += "<td> <b> Apellidos: </b></td><td>" + this.asientos[i].apellido + "<br></td></tr>";
-				html += "<td> <b> DNI: </b></td><td>" + this.asientos[i].dni + "<br></td></tr>";
+				html1 += `<tr>
+						<td> <b> <h3>N° de Asiento:</h3></b></td><td><br><h3><b>${(i+1)}</b></h3><br></td></tr>
+						<td> <b> Nombre: </b></td><td>${this.asientos[i].nombre}<br></td></tr>
+						<td> <b> Apellidos: </b></td><td>${this.asientos[i].apellido}<br></td></tr>
+						<td> <b> DNI: </b></td><td>${this.asientos[i].dni}<br></td></tr>`;
 			}
 		}
-		enlistando.innerHTML = html;
-		html += "</table><br><br>"
+		html1 += "</table><br><br>";
+		$("#enlistando").html(html1);
 		this.limpiar();
 	}
 	cancelar() {
 		console.log(this.numAsiento)
-		if (this.numAsiento == -1) {
-			alert("Seleccione primero el asiento a cancelar!")
-		} else if (this.celdaPress.style.backgroundColor == "red") {
-			//asientos.splice((numAsiento-1), 1);
+		if ((this.numAsiento == -1) || $("#nombre").val() == "" || $("#apellido").val() == "" || $("#dni").val() == "") {
+			swal('Oops...', 'Seleccione primero el asiento que quiere cancelar!', 'error');
+		}
+		else if (this.celdaPress.style.backgroundColor == "red") {
+			//asientos.splice((this.numAsiento - 1), 1);
 			console.log(this.asientos)
 			this.asientos[this.numAsiento - 1] = undefined;
 			this.celdaPress.style.backgroundColor = "transparent";
-			alert("El asiento ha sido cancelado");
+			swal({
+				title: "Cancelado!"
+				, text: "El asiento ha sido cancelado"
+				, type: "success"
+			});
 		}
 		this.limpiar();
 	}
 	limpiar() {
-		document.getElementById("mostrar").textContent = "";
-		document.getElementById("nombre").value = "";
-		document.getElementById("apellido").value = "";
-		document.getElementById("dni").value = "";
-		document.getElementById("buscarDni").value = "";
+		$("#mostrar").val("");
+		$("#nombre").val("");
+		$("#apellido").val("");
+		$("#dni").val("");
+		$("#buscarDni").val("");
 	}
 }
-let ejecuta = new Areolinea();
-ejecuta.dibuja();
-/*ejecuta.redirect();*/
-// ejecuta.buscar();
-// ejecuta.listar();
-// ejecuta.reservar();
-// ejecuta.cancelar();
-// ejecuta.limpiar();
+let avion = new Aerolinea();
+avion.inicio()
